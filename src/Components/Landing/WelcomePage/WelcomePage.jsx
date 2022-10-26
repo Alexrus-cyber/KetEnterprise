ade9074eaf02ec48ffbbe3a402409e2ab7e7e385
Alexrus-cyber <85308640+Alexrus-cyber@users.noreply.github.com>
2022-10-26 22:37:34 +0300

    Landing page update

--- src/Components/Landing/WelcomePage/WelcomePage.jsx
+++ src/Components/Landing/WelcomePage/WelcomePage.jsx
@@ -7,9 +7,9 @@ export const WelcomePage = () => {
     return (
             <div className={styles.container}>
                 <div className={styles.welcome}>
-                    <div className={styles.superTitle}>
+                    <div className={styles.superTitle}> {/*Первый заголовок*/}
                             <Title/>
-                        <div className={styles.buttonContainer}>
+                        <div className={styles.buttonContainer + ' ' + styles.flex}>
                             <p className={styles.p}>
                                 КЭТ — это серьёзная профессиональная подготовка и
                                 разносторонняя студенческая жизнь, это лекции, семинары и
@@ -20,29 +20,31 @@ export const WelcomePage = () => {
                                 <a style={{textDecoration: "none", color: "white"}} href={'http://spo-ket.ru/'}>  <button className={styles.button}>Вернуться на основной сайт</button></a>
                             </div>
                     </div>
-                        <div className={styles.second}>
+                        <div className={styles.second}> {/*Второй Галлерея*/}
                             <div className={styles.flex}>
                                 <div className={styles.inlineBlock}>
                                     <div className={styles.imageLap}>
                                         <img className={styles.imageLaptop} src={image} alt={''}/>
                                     </div>
+                                </div>
+                            </div> {/*Планшет*/}
+                            <div className={styles.flex}>
+                                <div className={styles.inlineBlock}>
+                                    <div className={styles.tablet}>
+                                        <img className={styles.imageTablet} src={image} alt={''}/>
+                                    </div>
                                     <div className={styles.imageLine}></div>
                                 </div>
-                            </div>
+                            </div> {/*Ноутбук*/}
                             <div>
                                 <div>
                                     <img className={styles.imageLaptop}  src={image1} alt={''}/>
                                 </div>
-                            </div>
-                            <div>
-                                <div>
-                                    <img className={styles.imageLaptop}  src={image1} alt={''}/>
-                                </div>
-                            </div>
+                            </div> {/*Телефон*/}
                         </div>
                         <div className={styles.third}>
 
-                        </div>
+                        </div> {/*Третий блок*/}
                 </div>
             </div>
 
